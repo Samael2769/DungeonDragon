c5346981eed12b9c109541c157c47336894dc76b
Jeremie Joannard <Samael@localhost.localdomain>
2022-01-24 16:55:43 +0100

    [ADD]imob

---
+++ IMob.hpp
@@ -0,0 +1,20 @@
+/*
+** EPITECH PROJECT, 2022
+** Untitled (Workspace)
+** File description:
+** IMob
+*/
+
+#ifndef IMOB_HPP_
+#define IMOB_HPP_
+
+class IMob {
+    public:
+        IMob();
+        ~IMob();
+
+    protected:
+    private:
+};
+
+#endif /* !IMOB_HPP_ */
